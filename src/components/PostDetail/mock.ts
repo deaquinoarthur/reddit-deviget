2cf46d05707cf0a9f9acc55ee5991fd8183e8eb1
Arthur D'Aquino <arthur.deaquino@jalasoft.com>
2021-09-26 21:49:03 -0300

    [feature/reddit-deviget] Set redux state on the PostDetais component

--- src/components/PostDetail/mock.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  author: 'washedupwornout',
-  thumbnail:
-    'http://b.thumbs.redditmedia.com/9N1f7UGKM5fPZydrsgbb4_SUyyLW7A27um1VOygY3LM.jpg',
-  title: "Man trying to return a dog's toy gets tricked into playing fetch",
-}
