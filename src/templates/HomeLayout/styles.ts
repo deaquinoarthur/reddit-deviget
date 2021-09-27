59a4a5249a3709088aa696883a3b69b669b2951c
Arthur D'Aquino <arthur.deaquino@jalasoft.com>
2021-09-27 20:35:05 -0300

    [feature/reddit-deviget] Fixed homelayout style issues

--- src/templates/HomeLayout/styles.ts
+++ src/templates/HomeLayout/styles.ts
@@ -6,15 +6,16 @@ export const HomeLayout = styled.main.attrs({
 })``
 
 export const Container = styled.div.attrs({
-  className: 'Container'
+  className: 'HomeLayout__Container'
 })`
-  display: grid;
-  grid-template-columns: minmax(380px, 1fr) 2fr;
-  width: 100%;
-  max-width: 1040px;
-  margin: 0 auto;
+  ${media.greaterThan('large')`
+    display: grid;
+    grid-template-columns: minmax(380px, 1fr) 2fr;
 
-  ${media.lessThan('medium')`
-    grid-template-columns: 1fr;
+    width: 100%;
+    max-width: 1040px;
+    margin: 0 auto;
+
+    overflow: hidden;
   `}
 `
