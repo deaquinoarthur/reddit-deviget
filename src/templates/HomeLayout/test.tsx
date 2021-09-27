582b4e8785dd6798d69f13d5b0c17a13913769f8
Arthur D'Aquino <arthur.deaquino@jalasoft.com>
2021-09-27 20:34:05 -0300

    [feature/reddit-deviget] Improved HomeLayout tests with redux store

--- src/templates/HomeLayout/test.tsx
+++ src/templates/HomeLayout/test.tsx
@@ -1,8 +1,17 @@
+import { Provider } from 'react-redux'
+
+import store from 'store'
+
 import { renderWithTheme } from 'utils/tests/helpers'
+
 import HomeLayout from '.'
 
 describe('<HomeLayout />', () => {
   it('should render HomeLayout correctly', () => {
-    renderWithTheme(<HomeLayout />)
+    renderWithTheme(
+      <Provider store={store}>
+        <HomeLayout />
+      </Provider>
+    )
   })
 })
