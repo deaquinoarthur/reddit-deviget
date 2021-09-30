82a7cf6a4319e66bc47aa6ff9377bf5b93f323cd
Arthur D'Aquino <arthur.deaquino@jalasoft.com>
2021-09-30 00:47:36 -0300

    [feature/reddit-deviget] Improved the fetchData function

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,25 +1,57 @@
 import { URLType } from 'types/posts'
 
 /**
- * Hook to fetch data from an API
+ * Hook to fetch data from an API and build a list with
+ * only needed attributes.
+ *
  * @param {string} children - The URL to fetch the data
  * @param {URLType} params - The query params of an URL
  * @returns {string}
  */
-export const fetchData = async (url: string, params: URLType) => {
-  const buildURLParams = Object.keys(params).reduce((acc, item, index) => {
-    if (index === 0) {
-      acc += `?${item}=${params[item]}`
-    } else {
-      acc += `&${item}=${params[item]}`
-    }
 
-    return acc
-  }, '')
+const postsWithOnlyNeededProperties = (dataFromResponse: any) => {
+  return dataFromResponse.map((item: typeof dataFromResponse) => ({
+    id: item.data.id,
+    numComments: item.data.num_comments,
+    thumbnail: item.data.thumbnail,
+    title: item.data.title,
+    created: item.data.created,
+    author: item.data.author,
+    clicked: item.data.clicked
+  }))
+}
+
+export const fetchData = async (url: string, params: URLType) => {
+  let after = ''
+  let before = ''
+
+  const buildURLParams = Object.keys(params).reduce(
+    (queryParams, item, index) => {
+      if (index === 0) {
+        queryParams += `?${item}=${params[item]}`
+      } else {
+        queryParams += `&${item}=${params[item]}`
+      }
+
+      return queryParams
+    },
+    ''
+  )
 
   url += buildURLParams
 
-  return await fetch(url)
+  const response = await fetch(url)
     .then((data) => data.json())
-    .then((res) => res)
+    .then((res) => {
+      after = res.data.after
+      before = res.data.before
+
+      return postsWithOnlyNeededProperties(res.data.children)
+    })
+
+  return {
+    after,
+    before,
+    posts: response
+  }
 }
